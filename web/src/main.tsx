8e700b3a1e15ffd502385c30a408b10366674cdd
NathalyaStefhany <nathalya.stefhany@hotmail.com>
2025-04-13 20:08:21 -0300

    feat: add Tailwind CSS and add colors for theming

--- web/src/main.tsx
+++ web/src/main.tsx
@@ -1,6 +1,7 @@
 import { StrictMode } from 'react';
 import { createRoot } from 'react-dom/client';
 import { App } from './app.tsx';
+import './styles.css';
 
 // eslint-disable-next-line @typescript-eslint/no-non-null-assertion
 createRoot(document.getElementById('root')!).render(
