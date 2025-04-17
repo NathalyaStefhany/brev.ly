ff0bb3631286a7cf3fd1610ebbdcc6bf4c1a9f76
NathalyaStefhany <nathalya.stefhany@hotmail.com>
2025-04-17 17:54:28 -0300

    feat: configure path aliases

--- web/vite.config.ts
+++ web/vite.config.ts
@@ -1,6 +1,7 @@
 import { defineConfig, loadEnv } from 'vite';
 import react from '@vitejs/plugin-react';
 import tailwindcss from '@tailwindcss/vite';
+import path from 'path';
 
 import { envSchema } from './src/envSchema';
 
@@ -12,6 +13,11 @@ export default defineConfig(({ mode }) => {
     plugins: [react(), tailwindcss()],
     server: {
       port: env.VITE_PORT,
+    },
+    resolve: {
+      alias: {
+        '@': path.resolve(__dirname, 'src'),
+      },
     },
   };
 });
