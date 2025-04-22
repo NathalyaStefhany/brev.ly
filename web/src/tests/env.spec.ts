69a63e70ec59eef0e1b771bdd5238144878ae5ca
Nathalya Stefhany <nathalya.stefhany@hotmail.com>
2025-04-22 17:13:58 -0300

    Feat web sentry (#9)
    * feat: sentry configuration
    
    * feat: add button error to see how sentry works
    
    * fix: button error
    
    * test: fix broken tests

--- web/src/tests/env.spec.ts
+++ web/src/tests/env.spec.ts
@@ -3,6 +3,11 @@ import { describe, it, expect, vi, beforeEach } from 'vitest';
 describe('env.ts tests', () => {
   beforeEach(() => {
     vi.resetModules();
+
+    vi.stubEnv('VITE_SENTRY_ORG', 'org');
+    vi.stubEnv('VITE_SENTRY_PROJECT', 'project');
+    vi.stubEnv('VITE_SENTRY_DSN', 'https://dsn.com');
+    vi.stubEnv('VITE_SENTRY_AUTH_TOKEN', 'token');
   });
 
   it('should load and validate environment variables correctly', async () => {
@@ -25,5 +30,31 @@ describe('env.ts tests', () => {
     vi.stubEnv('VITE_PORT', 'port');
 
     await expect(import('@/env')).rejects.toThrowError(/VITE_PORT/);
+  });
+
+  it('should throw an error if VITE_SENTRY_DSN is not a valid url', async () => {
+    vi.stubEnv('VITE_SENTRY_DSN', 'dsn');
+
+    await expect(import('@/env')).rejects.toThrowError(/VITE_SENTRY_DSN/);
+  });
+
+  it('should throw an error if VITE_SENTRY_ORG is empty', async () => {
+    vi.stubEnv('VITE_SENTRY_ORG', undefined);
+
+    await expect(import('@/env')).rejects.toThrowError(/VITE_SENTRY_ORG/);
+  });
+
+  it('should throw an error if VITE_SENTRY_PROJECT is empty', async () => {
+    vi.stubEnv('VITE_SENTRY_PROJECT', undefined);
+
+    await expect(import('@/env')).rejects.toThrowError(/VITE_SENTRY_PROJECT/);
+  });
+
+  it('should throw an error if VITE_SENTRY_AUTH_TOKEN is empty', async () => {
+    vi.stubEnv('VITE_SENTRY_AUTH_TOKEN', undefined);
+
+    await expect(import('@/env')).rejects.toThrowError(
+      /VITE_SENTRY_AUTH_TOKEN/,
+    );
   });
 });
