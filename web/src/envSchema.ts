d025beb9e1a6bf867ce74d7f7987291bff2f34ec
NathalyaStefhany <nathalya.stefhany@hotmail.com>
2025-04-22 15:25:57 -0300

    feat: sentry configuration

--- web/src/envSchema.ts
+++ web/src/envSchema.ts
@@ -2,4 +2,9 @@ import { z } from 'zod';
 
 export const envSchema = z.object({
   VITE_PORT: z.coerce.number().default(4000),
+
+  VITE_SENTRY_ORG: z.string(),
+  VITE_SENTRY_PROJECT: z.string(),
+  VITE_SENTRY_DSN: z.string().url(),
+  VITE_SENTRY_AUTH_TOKEN: z.string(),
 });
--- web/vite.config.ts
+++ web/vite.config.ts
@@ -1,4 +1,5 @@
 import { defineConfig, loadEnv } from 'vite';
+import { sentryVitePlugin } from '@sentry/vite-plugin';
 import react from '@vitejs/plugin-react';
 import tailwindcss from '@tailwindcss/vite';
 import path from 'path';
@@ -10,7 +11,15 @@ export default defineConfig(({ mode }) => {
   const env = envSchema.parse(loadEnv(mode, process.cwd(), 'VITE_'));
 
   return {
-    plugins: [react(), tailwindcss()],
+    plugins: [
+      react(),
+      tailwindcss(),
+      sentryVitePlugin({
+        authToken: env.VITE_SENTRY_AUTH_TOKEN,
+        org: env.VITE_SENTRY_ORG,
+        project: env.VITE_SENTRY_PROJECT,
+      }),
+    ],
     server: {
       port: env.VITE_PORT,
       watch: {
@@ -21,6 +30,9 @@ export default defineConfig(({ mode }) => {
       alias: {
         '@': path.resolve(__dirname, 'src'),
       },
+    },
+    build: {
+      sourcemap: true,
     },
   };
 });
