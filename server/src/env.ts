739e3f73eed2d82e625ba9907eef68412b1abddc
NathalyaStefhany <nathalya.stefhany@hotmail.com>
2025-04-24 21:09:40 -0300

    feat: implement PostgreSQL database schema and configuration with drizzle

--- server/src/env.ts
+++ server/src/env.ts
@@ -1,10 +1,12 @@
 import { z } from 'zod';
 
 const envSchema = z.object({
-  POSTGRES_PORT: z.coerce.number().default(3333),
+  PORT: z.coerce.number().default(3333),
+  POSTGRES_PORT: z.coerce.number().default(5432),
   POSTGRES_USER: z.string(),
   POSTGRES_PASSWORD: z.string(),
   POSTGRES_DB: z.string(),
+  DATABASE_URL: z.string().url().startsWith('postgresql://'),
 });
 
 export const env = envSchema.parse(process.env);
