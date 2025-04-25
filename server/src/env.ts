b9008449075976374a8d6f9f5a43b631c9943089
Nathalya Stefhany <nathalya.stefhany@hotmail.com>
2025-04-24 21:23:05 -0300

    Feat postgresql (#11)
    * feat: add docker-compose to use postgresql db
    
    * feat: implement PostgreSQL database schema and configuration with drizzle
    
    * docs: update README to include Fastify, PostgreSQL, and Drizzle in tech stack

---
+++ server/src/env.ts
@@ -0,0 +1,12 @@
+import { z } from 'zod';
+
+const envSchema = z.object({
+  PORT: z.coerce.number().default(3333),
+  POSTGRES_PORT: z.coerce.number().default(5432),
+  POSTGRES_USER: z.string(),
+  POSTGRES_PASSWORD: z.string(),
+  POSTGRES_DB: z.string(),
+  DATABASE_URL: z.string().url().startsWith('postgresql://'),
+});
+
+export const env = envSchema.parse(process.env);
