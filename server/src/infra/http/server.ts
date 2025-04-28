36296451e8ab16bc5edd68fcb40b3d812360681a
NathalyaStefhany <nathalya.stefhany@hotmail.com>
2025-04-28 20:15:04 -0300

    feat: add Swagger documentation and enhance health check route

--- server/src/infra/http/server.ts
+++ server/src/infra/http/server.ts
@@ -1,11 +1,40 @@
-import fastifyCors from '@fastify/cors';
 import fastify from 'fastify';
+import fastifyCors from '@fastify/cors';
+import fastifySwagger from '@fastify/swagger';
+import scalarUI from '@scalar/fastify-api-reference';
+import {
+  jsonSchemaTransform,
+  serializerCompiler,
+  validatorCompiler,
+} from 'fastify-type-provider-zod';
 import { env } from '@/env';
 import { healthCheckRoute } from '@/infra/http/routes/health-check';
 
 const server = fastify();
 
+server.setValidatorCompiler(validatorCompiler);
+server.setSerializerCompiler(serializerCompiler);
+
 server.register(fastifyCors, { origin: '*' });
+
+server.register(fastifySwagger, {
+  openapi: {
+    info: {
+      title: 'Brev.ly',
+      description:
+        'API for creating and managing shortened links. It allows you to shorten long URLs and retrieve information about the generated links.',
+      version: '1.0.0',
+    },
+  },
+  transform: jsonSchemaTransform,
+});
+
+server.register(scalarUI, {
+  routePrefix: '/docs',
+  configuration: {
+    layout: 'modern',
+  },
+});
 
 server.register(healthCheckRoute);
 
