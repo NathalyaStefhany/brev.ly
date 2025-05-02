9573b42a0b326924412b78c6be66c68ddc914c14
NathalyaStefhany <nathalya.stefhany@hotmail.com>
2025-05-02 07:17:51 -0300

    feat: implement delete shortened link functionality and associated tests

---
+++ server/src/app/functions/delete-shortened-link.ts
@@ -0,0 +1,30 @@
+import { ShortenedLinkNotAvailable } from '@/app/errors/shortened-link-not-available';
+import { db } from '@/infra/db';
+import { schema } from '@/infra/db/schemas';
+import { Either, makeLeft, makeRight } from '@/shared/either';
+import { eq } from 'drizzle-orm';
+
+type DeleteShortenedLinkOutput = {
+  id: string;
+  originalLink: string;
+  shortenedLink: string;
+  quantityAccesses: number;
+  createdAt: Date;
+};
+
+export async function deleteShortenedLink(
+  id: string,
+): Promise<Either<ShortenedLinkNotAvailable, DeleteShortenedLinkOutput>> {
+  const shortenedLinksSchema = schema.shortenedLinks;
+
+  const deletedInfo = await db
+    .delete(shortenedLinksSchema)
+    .where(eq(shortenedLinksSchema.id, id))
+    .returning();
+
+  if (deletedInfo.length) {
+    return makeRight(deletedInfo[0]);
+  }
+
+  return makeLeft(new ShortenedLinkNotAvailable());
+}
---
+++ server/src/infra/http/routes/delete-shortened-link.spec.ts
@@ -0,0 +1,43 @@
+import { afterAll, beforeAll, describe, expect, it } from 'vitest';
+import { randomUUID } from 'node:crypto';
+import { server } from '@/infra/http/server';
+import { db } from '@/infra/db';
+import { schema } from '@/infra/db/schemas';
+
+describe('Delete shortened link route', () => {
+  beforeAll(async () => {
+    await server.ready();
+  });
+
+  afterAll(async () => {
+    await server.close();
+  });
+
+  it('should return 204 when delete a shortened link', async () => {
+    const shortenedLink = randomUUID().replaceAll('-', '');
+
+    const [shortenedLinkInfo] = await db
+      .insert(schema.shortenedLinks)
+      .values({ originalLink: 'https://test.com', shortenedLink })
+      .returning();
+
+    const response = await server.inject({
+      method: 'DELETE',
+      url: `/shortened-links/${shortenedLinkInfo.id}`,
+    });
+
+    expect(response.statusCode).toBe(204);
+  });
+
+  it('should return 404 when trying to delete a shortened link that does not exist', async () => {
+    const response = await server.inject({
+      method: 'DELETE',
+      url: '/shortened-links/1',
+    });
+
+    const jsonResponse = response.json();
+
+    expect(response.statusCode).toBe(404);
+    expect(jsonResponse.message).toBe('Shortened link does not exist');
+  });
+});
