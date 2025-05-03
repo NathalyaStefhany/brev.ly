8bbfdf77c816da3965303a474e37cc170ea4d8bb
NathalyaStefhany <nathalya.stefhany@hotmail.com>
2025-05-03 19:38:32 -0300

    test: fix broken tests

--- web/src/tests/routes.spec.tsx
+++ web/src/tests/routes.spec.tsx
@@ -1,6 +1,8 @@
 import { describe, expect, it } from 'vitest';
 import { createMemoryRouter, RouterProvider } from 'react-router';
 import { act, render, screen } from '@testing-library/react';
+import { QueryClientProvider } from '@tanstack/react-query';
+import { queryClient } from '@/service/queryClient';
 import { routesConfig } from '@/routes';
 
 describe('Routes tests', () => {
@@ -22,7 +24,11 @@ describe('Routes tests', () => {
 
     // eslint-disable-next-line @typescript-eslint/require-await
     await act(async () => {
-      render(<RouterProvider router={router} />);
+      render(
+        <QueryClientProvider client={queryClient}>
+          <RouterProvider router={router} />
+        </QueryClientProvider>,
+      );
     });
 
     expect(screen.queryByTestId('container-not-found')).not.toBeInTheDocument();
